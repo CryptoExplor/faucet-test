2f09931b532449cf29ba3b92433969ac91b8f7f4
RaviK <ravikumar699121@gmail.com>
2025-08-03 10:09:08 +0000

    import { defineConfig } from "drizzle-kit";
    if (!process.env.DATABASE_U

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+
+import { defineConfig } from "drizzle-kit";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL, ensure the database is provisioned");
+}
+
+export default defineConfig({
+  out: "./migrations",
+  schema: "./src/lib/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+});
