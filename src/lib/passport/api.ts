f43a7c4bed37eccfb25e201719e59e1c0907a8e7
RaviK <ravikumar699121@gmail.com>
2025-08-05 11:49:10 +0000

    import axios from "axios"; import { Passport } from "./types"; import {

--- src/lib/passport/api.ts
+++
@@ -1,5 +0,0 @@
-
-// This file is no longer needed as API calls are handled server-side in actions.ts.
-// It is kept to prevent import errors but should be considered deprecated.
-// All logic has been moved to src/app/actions.ts in the getPassportScore function.
-export {};
