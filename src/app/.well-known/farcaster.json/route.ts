8054d390677e15025db170358f09ba0092740d06
CryptoExplor <102873939+CryptoExplor@users.noreply.github.com>
2025-11-01 13:50:30 +0530

    Update route.ts

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -1,7 +1,7 @@
 
 import { NextRequest, NextResponse } from "next/server";
 
-const FARCASTER_HOSTED_MANIFEST_URL = "https://api.farcaster.xyz/miniapps/hosted-manifest/01987153-3fe0-e658-7f1a-ae5b3fd62d86";
+const FARCASTER_HOSTED_MANIFEST_URL = "https://api.farcaster.xyz/miniapps/hosted-manifest/019a3e80-9377-059d-a154-415fa065fa57";
 
 export async function GET(req: NextRequest) {
     // Redirect to the hosted manifest file
